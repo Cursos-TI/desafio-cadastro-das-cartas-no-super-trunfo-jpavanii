bc2c2bfe844ad70d22e0af36314c3082046c932b
jpavanii <pavanijao@gmail.com>
2025-03-12 20:58:04 -0300

    Update CartasSuperTrunfo.c
    ADD, FUNÇÕES DO DESAFIO AVENTUREIRO, INTERMEDIÁRIO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,8 @@ int População_1;
 float Área_1; 
 float PIB_1;
 int Pontos_Turísticos_1;
+float densiade_demografica_1;
+float pib_per_capita_1;
 
 
 
@@ -37,6 +39,9 @@ scanf("%f", &PIB_1);
 printf("digite os Pontos Turísticos: \n   ");
 scanf("%d", &Pontos_Turísticos_1);
 
+densiade_demografica_1 = População_1 / Área_1;
+pib_per_capita_1 = PIB_1 / População_1;
+
 
 
 printf("Carta 1: \n");
@@ -46,8 +51,8 @@ printf("Cidade: %s \n", Cidade_1);
 printf("População: %d \n", População_1);
 printf("Área: %f \n", Área_1);
 printf("PIB: %f \n", PIB_1);
-
-
+printf("Densidade Demográfica: %f \n", densiade_demografica_1);
+printf("PIB per capita: %f \n", pib_per_capita_1);
 
 
 char Estado_2[3];
@@ -57,6 +62,9 @@ int População_2;
 float Área_2; 
 float PIB_2;
 int Pontos_Turísticos_2;
+float densiade_demografica_2;
+float pib_per_capita_2;
+
 
 
 printf("Agora digite os dados da sengunda carta \n");
@@ -82,6 +90,9 @@ scanf("%f", &PIB_2);
 printf("digite os Pontos Turísticos: \n   ");
 scanf("%d", &Pontos_Turísticos_2);
 
+densiade_demografica_2 = População_2 / Área_2;
+pib_per_capita_2 = PIB_2 / População_2;
+
 
 printf("Carta 2: \n");
 printf("Estado: %s \n", Estado_2);
@@ -90,8 +101,18 @@ printf("Cidade: %s \n", Cidade_2);
 printf("População: %d \n", População_2);
 printf("Área: %f \n", Área_2);
 printf("PIB: %f \n", PIB_2);
+printf("Densidade Demográfica: %f \n", densiade_demografica_2);
+printf("PIB per capita: %f \n", pib_per_capita_2);
 
 
 return 0;
+
+
+
+
+
+
+
+
 
 }
