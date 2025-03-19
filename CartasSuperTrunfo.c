5e20f0f8ac152d757d2dc6556a5b4e5e55a8f34d
jpavanii <pavanijao@gmail.com>
2025-03-19 19:55:34 -0300

    Update CartasSuperTrunfo.c
    NIVEL MESTRE PAPAI

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,6 +12,12 @@ int Pontos_Turísticos_1;
 float densiade_demografica_1;
 float pib_per_capita_1;
 
+int Resultado_área;
+int Resultado_população;
+int Resultado_pib;
+int Resultado_pontos_turísticos;
+int Resultado_densidade_demográfica;
+int Resultado_pib_per_capita;
 
 
 
@@ -105,12 +111,30 @@ printf("Densidade Demográfica: %f \n", densiade_demografica_2);
 printf("PIB per capita: %f \n", pib_per_capita_2);
 
 
+
+
+
+
+
+printf("Agora é a hora da verdade, quem venceu? \n");
+
+Resultado_área = Área_1 > Área_2;
+Resultado_população = População_1 > População_2;
+Resultado_pib = PIB_1 > PIB_2;
+Resultado_pontos_turísticos = Pontos_Turísticos_1 > Pontos_Turísticos_2;
+Resultado_densidade_demográfica = densiade_demografica_1 > densiade_demografica_2;
+Resultado_pib_per_capita = pib_per_capita_1 > pib_per_capita_2;
+
+printf("Resultado da Área: %d \n", Resultado_área);
+printf("Resultado da População: %d \n", Resultado_população);
+printf("Resultado do PIB: %d \n", Resultado_pib);
+printf("Resultado dos Pontos Turísticos: %d \n", Resultado_pontos_turísticos);
+printf("Resultado da Densidade Demográfica: %d \n", Resultado_densidade_demográfica);
+printf("Resultado do PIB per capita: %d \n", Resultado_pib_per_capita);
+
+
+
 return 0;
-
-
-
-
-
 
 
 
